d6e44cdac9e18a31a58d8d49bece95e37f893653
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2018-06-26 13:43:41 +0200

    Add node installation and refreshing

--- dependencies.sh
+++ dependencies.sh
@@ -63,8 +63,11 @@ function insSdkman {
 }
 function insNvmNode {
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+    nvm install node --lts
     . ~/.bashrc
-    sudo nvm install node --lts
 }
 function insPostgres {
     sudo aptitude install postgresql, pgadmin3
