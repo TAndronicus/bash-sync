169dc5c2e50dabaabb2fcf480fda992d8b81d374
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2018-06-29 17:23:05 +0200

    Add package updating

---
+++ update.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+declare -a FUNCTION
+FUNCTION[1]="updPip"
+declare -a PROGRAMS
+PROGRAMS[1]="pip"
+
+function updPip {
+    pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+}
+
+for ((i=1;i<=${#PROGRAMS[@]};i++))
+do
+    printf "\nUpdate ${PROGRAMS[$i]}? [ynq]"
+    read ins
+    while [ $ins != 'y' ] && [ $ins != 'n' ] && [ $ins != 'q' ]
+    do
+        echo y - yes
+        echo n - no
+        echo q - quit
+        echo Update ${PROGRAMS[$i]}?
+        read ins
+    done
+    if [ $ins == 'y' ] || [ $ins == 'Y' ]
+    then
+        ${FUNCTION[$i]}
+    elif [ $ins == 'n' ]
+    then
+        echo Skipping ${PROGRAMS[$i]} update.
+    elif [ $ins == 'q' ]
+    then
+        echo Quitting installer
+        exit 1
+    fi
+done
