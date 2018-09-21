5a010d276c136ff76d890b291b7757cb17dd6781
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2018-09-21 19:33:13 +0200

    Add sdk update

--- update-packages.sh
+++ update-packages.sh
@@ -19,7 +19,9 @@ function updYarn {
     yarn global upgrade
 }
 function updSdkman {
-    eval "sdk update"
+    export SDKMAN_DIR="$HOME/.sdkman"
+    [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+    sdk update
 }
 function updVim {
     vim +PlugUpdate +qall
