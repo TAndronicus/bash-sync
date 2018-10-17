a3d987fc7d664946d9e1f703fccb4c56f82361f3
Jędrzej Biedrzycki <jed.biedrzycki@gmail.com>
2018-10-17 14:55:08 +0200

    Add catalog for phd in sync

--- sync.sh
+++ sync.sh
@@ -4,6 +4,8 @@ CI="/media/jb/145C91ED5C91CA3E/Workspace/Python/Classification/results"
 WORK="/media/jb/145C91ED5C91CA3E/Dokumenty/Praca"
 DATASETS="/media/jb/145C91ED5C91CA3E/Workspace/Python/Classification/datasets"
 MGR="/media/jb/145C91ED5C91CA3E/Dokumenty/Uczelnia/Praca magisterska"
+DOC="/media/jb/145C91ED5C91CA3E/Dokumenty/Uczelnia/Doktorat"
+
 
 function syncCi {
     java -jar /opt/jdrivesync/jdrivesync.jar $2 -r CI -l "$CI"
@@ -21,6 +23,10 @@ function syncMgr {
     java -jar /opt/jdrivesync/jdrivesync.jar $2 -r mgr -l "$MGR"
 }
 
+function syncDoktorat {
+    java -jar /opt/jdrivesync/jdrivesync.jar $2 -r doktorat -l "$DOC"
+}
+
 if [[ $1 = "ci" ]]; then
     syncCi
 elif [[ $1 = "praca" ]]; then
@@ -29,6 +35,8 @@ elif [[ $1 = "datasets" ]]; then
     syncDatasets
 elif [[ $1 = "mgr" ]]; then
     syncMgr
+elif [[ $1 = "doktorat" ]]; then
+    syncDoktorat
 else
     echo syntax: bash sync.sh catalog -d/u
     echo catalog is one of following:
@@ -36,4 +44,5 @@ else
     echo praca - katalog z dokumentami do pracy
     echo datasets - bazy danych do klasyfikacji
     echo mgr - praca magisterska
+    echo doktorat - katalog z dokumentami do doktoratu
 fi
